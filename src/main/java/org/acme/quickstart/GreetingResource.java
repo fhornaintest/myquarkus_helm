6b2d93b70b2d08258914c373ad43416e0ad05dc6
fhornaintest <35542169+fhornaintest@users.noreply.github.com>
2026-01-28 13:01:39 +0100

    Update GreetingResource.java

--- src/main/java/org/acme/quickstart/GreetingResource.java
+++ src/main/java/org/acme/quickstart/GreetingResource.java
@@ -1,16 +1,29 @@
 package org.acme.quickstart;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @Inject
+    GreetingService service;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greeting(@PathParam String name) {
+        return service.greeting(name);
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return "hello";
     }
 }
